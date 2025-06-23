d8d2ab3b277cd15a10842b3631f13f596f0a2229
Ttho0mas <1234gtoto@gmail.com>
2025-06-23 18:39:12 -0300

    Programacion de footer mas images

---
+++ vista/footer.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="/css/estilo_footer.css">
+</head>
+
+<body>
+    <footer class="pie-pagina">
+        <div class="pie-pagina-a">
+
+            <div class="seccion">
+                <h3>Contacto</h3>
+                <ul>
+                    <li>Contáctenos</li>
+                    <li>Microfix@gmail.com</li>
+                    <li>099000000|099000009</li>
+                </ul>
+            </div>
+            <div class="seccion">
+               
+            </div>
+            <div class="seccion">
+                <h3>Enlaces</h3>
+                <ul>
+                    <li><a href="#">Terminos y Condiciones</a></li>
+                    <li><a href="#">Locales de Microfix</a></li>
+                </ul>
+                <div class="red-social">
+                    <a href="#"><img src="/img/icon-whatsapp.png" style="width: 20px;" alt="Icono de Whatsapp"></a>
+                    <a href="#"><img src="/img/icon-instagram.png" style="width: 20px;" alt="Icono de Instagram"></a>
+                    <a href="#"><img src="/img/icon-facebook.png" style="width: 20px;" alt="Icono de Facebook"></a>
+                    <a href="#"><img src="/img/icon-Tiktok.png" style="width: 20px;" alt="Icono de Tiktok"></a>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="pie-pagina-b">
+    <div class="left">
+        &copy; 2025 Microfix Todos los derechos reservados.
+    </div>
+    <div class="right">
+        Desarrollado por <b>NanoCatkz</b>
+        <img src="/img/logo-nanocatxz.png" alt="logo de NanoCatxz">
+    </div>
+</div>
+    </footer>
+</body>
+
+</html>
